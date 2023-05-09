b3bcf5ab2feccc807749daf2b00d8e3da92ad1b2
Rufina15 <82505991+Rufina15@users.noreply.github.com>
2023-05-09 21:30:54 +0600

    fixed division

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -65,14 +65,13 @@ class MainActivity : AppCompatActivity() {
                 val no1 = edtNo1.text.toString().toInt()
                 val no2 = edtNo2.text.toString().toInt()
 
-                var div : Int
+                var div : Float
                 if (no2==0){
-                    div = 0
                     Toast.makeText(this, "A number can't be divided by zero as produces infinte solutions",Toast.LENGTH_LONG).show();
                 }else {
-                    div = no1 / no2
+                    div = (no1.toFloat() / no2)
+                    Toast.makeText(this, "The Division is $div",Toast.LENGTH_LONG).show();
                 }
-                Toast.makeText(this, "The Division is $div",Toast.LENGTH_LONG).show();
 
             }else{
                 Toast.makeText(this, "Please Fill all the Required Blanks.",Toast.LENGTH_LONG).show();
